0b414045d49c0f62280b14c86adb2b76b2383a93
Wendel Silva <wendelmacapa@gmail.com>
2021-02-11 08:50:39 -0300

    Projeto iniciado, mostrando a mensagem ola mundo, bem vindo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Program_Test {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Bem-Vindo!");
+        }
+    }
+}
